49cc56d8fe7bd5a1acf7dbc8d5dab17d1367c1c3
JCampy <93626334+JCampy@users.noreply.github.com>
2021-11-18 01:29:00 -0600

    Added Category.java - Create / Store / Retrieve categorys Added ProjUIController.Java - Just a simple outline for prep Added Task.java - All task requirements for the create page on ProjUI.xml file create page Added TaskCategory.java - WIP HashMap for assigning a task to a category I still need some ideas on how to go about assigning the task to the category as well.

---
+++ CIS285Project/src/cis285project/Category.java
@@ -0,0 +1,28 @@
+/*
+ * Category class is created to make categorys and set them in the
+ * create section of the proJUI.fxml file and other aspects
+ * 
+ */
+package cis285project;
+
+/**
+ *
+ * @author Jason
+ */
+
+public class Category {
+    
+    private String categoryName; // String to hold the name of a Category
+    
+    public Category(String cN){ // Constructor that takes cN as a parameter and sets it equal to categoryName
+        categoryName = cN;
+    }
+    
+    public void setCategoryName(String cN){ // Method for setting a category's name
+        categoryName = cN;
+    }
+    public String getCategoryName(){ // Method for retrieving the name of a category
+        return categoryName;
+    }
+    
+}
---
+++ CIS285Project/src/cis285project/TaskCategoryMap.java
@@ -0,0 +1,21 @@
+/*
+ * Class for assigning a task to a 
+ * category using HashMap
+ * 
+ */
+package cis285project;
+
+/**
+ *
+ * @author Jason
+ */
+public class TaskCategoryMap {
+    
+    /*
+     *Creats new HashMap that is two strings the key being the category and the
+     * second being the task. Allowing us to know what task a category has inside of it.
+     */
+    HashMap<String, String> = taskCat = new HashMap<String, String>(); 
+    
+    public void setCatTask( ) // WIP still need to think of a way to actually assign as task to a category. Might require GUI controller first to directly assign it?
+}
