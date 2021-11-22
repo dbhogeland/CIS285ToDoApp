7c2bc31cc5d2e8b4ffa53a2aa911e6a78c510846
jdchunn <jdchunn9280@gmail.com>
2021-11-21 20:33:35 -0600

    ActiveTask

---
+++ ActiveTask.java
@@ -0,0 +1,55 @@
+package cis285project;
+
+public class Task {
+
+    public String Category1;
+    public String Category2;
+    public String alltask;
+    public String Category1ShortDesc;
+    public String Category2ShortDesc;
+
+    Category1 = C1;
+    Category2 = C2;
+    allTask = at;
+    Category1ShortDesc = C1Sd;
+    Category2ShortDesc = C1Sd;
+
+
+public void setCategory1(String C1){
+    Category1 = C1;
+}
+    public String getCategory1(){
+        return Category1;
+}
+
+    public void setCategory2(String C2){
+    Category2 = C2;
+}
+
+    public String getCategory2(){
+        return Category2;
+}
+
+    public void setAlltask(String at){
+        allTask = at;
+    }
+
+    public String getAlltask(){
+        return allTask;
+    }
+
+    public void setCategory1ShortDesc(String c1Sd){
+        Category1ShortDesc = C1Sd;
+    }
+
+    public String getCategory1ShortDesc(){
+        return Category1ShortDesc
+    }
+
+    public void setCategory2ShortDesc(String c2Sd){
+        Category2ShortDesc = C2Sd;
+    }
+
+    public String getCategory2ShortDesc(){
+        return Category2ShortDesc
+    }
