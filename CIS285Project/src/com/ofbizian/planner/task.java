725573ad31f2b3a5489be8683be8b98af02e241c
mehakjaved <mehakjaved@mehaks-mbp.lan1>
2021-11-28 22:36:43 -0600

    Account Creation for the User.

---
+++ CIS285Project/src/com/ofbizian/planner/task.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author mehakjaved
+ */
+package com.ofbizian.planner;
+
+public class Task {
+	private final String id;
+
+    public Task(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Task task = (Task) o;
+
+        if (id != null ? !id.equals(task.id) : task.id != null) return false;
+
+        return true;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
+
+    @Override
+    public String toString() {
+        return "Task{" +
+                "id='" + id + '\'' +
+                '}';
+    }
+}
